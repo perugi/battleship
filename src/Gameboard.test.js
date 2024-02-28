3fcce8b8750167debd6c7232cca05528a70897f6
perugi <dominik.perusko@gmail.com>
2024-02-28 11:44:26 +0100

    Add error messages to throw tests, correct out of bounds tests

--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -9,11 +9,11 @@ test('create an empty board', () => {
 test('creating a board with dimension less than 1 throws an error', () => {
   expect(() => {
     Gameboard(0);
-  }).toThrow();
+  }).toThrow('Gameboard dimension must be greater than 0');
 
   expect(() => {
     Gameboard(-1);
-  }).toThrow();
+  }).toThrow('Gameboard dimension must be greater than 0');
 });
 
 test('create a board with a single ship', () => {
@@ -29,18 +29,24 @@ test('create a board with multiple ships', () => {
   expect(gameboard.getShipCoordinates()).toEqual([
     [0, 0],
     [1, 0],
-    [1, 0],
+    [2, 0],
   ]);
 });
 
 test('placing a ship out of bounds throws an error', () => {
   const gameboard = Gameboard(10);
   expect(() => {
+    gameboard.placeShip(1, -1, 0, 'v');
+  }).toThrow('Ship out of bounds');
+  expect(() => {
+    gameboard.placeShip(1, 0, -1, 'v');
+  }).toThrow('Ship out of bounds');
+  expect(() => {
     gameboard.placeShip(1, 10, 0, 'v');
-  }).toThrow();
+  }).toThrow('Ship out of bounds');
   expect(() => {
     gameboard.placeShip(2, 9, 0, 'h');
-  }).toThrow();
+  }).toThrow('Ship out of bounds');
 });
 
 test('placing a ship on top of another ship throws an error', () => {
@@ -48,40 +54,40 @@ test('placing a ship on top of another ship throws an error', () => {
   gameboard.placeShip(1, 0, 0, 'v');
   expect(() => {
     gameboard.placeShip(2, 0, 0, 'v');
-  }).toThrow();
+  }).toThrow('Placed ship collides with an existing ship');
 });
 
 test('placing a ship with an invalid length throws an error', () => {
   const gameboard = Gameboard(10);
   expect(() => {
     gameboard.placeShip(0, 0, 0, 'v');
-  }).toThrow();
+  }).toThrow('Ship length must be greater than 0');
   expect(() => {
     gameboard.placeShip(-1, 0, 0, 'v');
-  }).toThrow();
+  }).toThrow('Ship length must be greater than 0');
   expect(() => {
     gameboard.placeShip('a', 0, 0, 'v');
-  });
+  }).toThrow('Ship length must be an integer');
 });
 
 test('placing a ship with an invalid coordinate throws an error', () => {
   const gameboard = Gameboard(10);
   expect(() => {
     gameboard.placeShip(1, '0', 0, 'v');
-  }).toThrow();
+  }).toThrow('X coordinate must be an integer');
   expect(() => {
     gameboard.placeShip(1, 0, '0', 'v');
-  }).toThrow();
+  }).toThrow('Y coordinate must be an integer');
 });
 
 test('placing a ship with an invalid direction throws an error', () => {
   const gameboard = Gameboard(10);
   expect(() => {
     gameboard.placeShip(1, 0, 0, 'x');
-  }).toThrow();
+  }).toThrow('Direction must be either "v" or "h"');
   expect(() => {
     gameboard.placeShip(1, 0, 0, 1);
-  }).toThrow();
+  }).toThrow('Direction must be either "v" or "h"');
 });
 
 test('remove a ship', () => {
@@ -95,7 +101,7 @@ test('removing an unexistent ship throws an error', () => {
   const gameboard = Gameboard(10);
   expect(() => {
     gameboard.removeShip(1, 0, 0, 'v');
-  }).toThrow();
+  }).toThrow('Ship not found');
 });
 
 // test('hit a ship with an attack', () => {
