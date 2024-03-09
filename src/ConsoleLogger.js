da172d0d96dc1dd0921d733fe25158cc5b1e7cc1
perugi <dominik.perusko@gmail.com>
2024-03-09 13:09:34 +0100

    Cover cases when arguments not defined

--- src/ConsoleLogger.js
+++ src/ConsoleLogger.js
@@ -8,11 +8,13 @@ const ConsoleLogger = (events) => {
   events.on('gameStateChange', (data) => {
     let log = '--- Game State Change ---\n';
     log += `Game State: ${data.gameState}\n`;
-    log += `Shot: ${data.shot ?? 'null'}\n`;
+    log += `Shot: ${JSON.stringify(data.shot) ?? 'null'}\n`;
     log += `Ship Hit: ${data.shipHit ?? 'null'}\n`;
     log += `Player 1: ${data.player1.toString()}\n`;
     log += `Player 2: ${data.player2.toString()}\n`;
-    log += `Active Player: ${data.activePlayer.getName()}\n`;
+    log += `Active Player: ${
+      data.activePlayer ? data.activePlayer.getName() : 'null'
+    }\n`;
     log += `Winner: ${data.winner ? data.winner.getName() : 'null'}\n`;
     console.log(log);
   });
